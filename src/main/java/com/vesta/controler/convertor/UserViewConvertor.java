99d14b98504456903808d0848a35a1063609331f
gbusila <gbusila@pentalog.com>
2019-05-28 10:53:50 +0300

    VT-8

--- src/main/java/com/vesta/controler/convertor/UserViewConvertor.java
+++ src/main/java/com/vesta/controler/convertor/UserViewConvertor.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class UserViewConvertor {
 
     public UserView convertor (UserDto dto){
+        if(dto == null) return null;
         UserView view = new UserView();
         view.setUsername(dto.getUsername());
         view.setEmail(dto.getEmail());
