2f9184a47f9045925d286a68c25bda06106bf29a
gbusila <gbusila@pentalog.com>
2019-06-06 17:27:16 +0300

    VT-9

---
+++ src/main/java/com/vesta/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+
+package com.vesta.service;
+
+import com.vesta.repository.UserRepository;
+import com.vesta.service.converter.UserConverter;
+import com.vesta.service.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserConverter userConverter;
+
+    public UserDto getById(Long id) {
+        return userConverter.converter(userRepository.findById(id).orElse(null));
+    }
+}
