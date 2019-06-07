51797727716fe58cde871c064c2c6184803adfc2
Lindvior2 <sarpe36@gmail.com>
2019-06-07 13:53:42 +0300

    Refactor for VT-10 Authentification debug

--- src/main/java/com/vesta/repository/UserRepository.java
+++ src/main/java/com/vesta/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.vesta.repository;
 
 import com.vesta.repository.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +13,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
 
-    boolean existsByUsernameOrEmailAndPassword(String username, String email, String password);
+    @Query("select case when count(u)> 0 then true else false end from UserEntity u where u.password=:password " +
+            "and (u.email=:email or u.username=:username)")
+    boolean existsByUsernameOrEmailAndPassword(@Param("username") String username,
+                                               @Param("email") String email,
+                                               @Param("password") String password);
 }
