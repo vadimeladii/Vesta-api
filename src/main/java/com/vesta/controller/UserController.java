7882209d9acc07fb1e0399d807028c388809aa2a
gbusila <gbusila@pentalog.com>
2019-06-19 11:26:14 +0300

    VT-16

--- src/main/java/com/vesta/controller/UserController.java
+++ src/main/java/com/vesta/controller/UserController.java
@@ -3,6 +3,10 @@ package com.vesta.controller;
 import com.vesta.controller.view.Token;
 import com.vesta.controller.view.UserView;
 import com.vesta.service.dto.AccountCredential;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,14 +14,28 @@ import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/user")
+@Api(value = "User Controller REST Endpoint", description = "Shows the user info")
 public interface UserController {
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Login was success"),
+            @ApiResponse(code = 404, message = "Username, password or email not found")
+    })
     @PostMapping("/login")
     Map<String, Token> login(@RequestBody AccountCredential accountCredential);
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get user by id has succeeded"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
     @GetMapping("/{id}")
     UserView getById(@PathVariable Long id);
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get all users has succeeded"),
+            @ApiResponse(code = 404, message = "Users not found")
+    })
     @GetMapping
     List<UserView> findAll();
 
