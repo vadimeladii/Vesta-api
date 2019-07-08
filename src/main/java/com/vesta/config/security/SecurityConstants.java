99ee83abf923c56f7a826780901f95c45a9c13dd
Vadim Eladii <veladii@straighterline.com>
2019-07-08 13:05:33 +0300

    Change expiration Time for token

--- src/main/java/com/vesta/config/security/SecurityConstants.java
+++ src/main/java/com/vesta/config/security/SecurityConstants.java
@@ -5,7 +5,7 @@ public class SecurityConstants {
     public static final String JWT_SECRET = "n2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-J@NcRf";
     public static final String REFRESH_SECRET = "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
     public static final String TOKEN_HEADER = "X-Token-Authentication";
-    public static final long EXPIRATION_TIME = 900_000;
+    public static final long EXPIRATION_TIME = 1800_000;
     public static final long REFRESH_EXPIRATION = 86_400_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String API_KEY_NAME = "JWT";
