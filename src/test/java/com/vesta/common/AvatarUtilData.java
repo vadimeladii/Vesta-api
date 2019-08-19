a8840b7c0713b3c7ea5e17cbc66a11a37a890347
Lindvior2 <sarpe36@gmail.com>
2019-08-19 17:03:02 +0300

    Addititonal and User Avatar

--- src/test/java/com/vesta/common/AvatarUtilData.java
+++ src/test/java/com/vesta/common/AvatarUtilData.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang3.RandomUtils;
 
 public class AvatarUtilData {
 
-    private static Long AVATAR_ID = Long.parseLong(RandomStringUtils.randomNumeric(10));
+    public static Long AVATAR_ID = Long.parseLong(RandomStringUtils.randomNumeric(10));
     public static String AVATAR_NAME = RandomStringUtils.randomAlphabetic(10);
     private static byte[] AVATAR_BYTE = RandomUtils.nextBytes(20);
     private static UserEntity USER_ID = new UserEntity();
